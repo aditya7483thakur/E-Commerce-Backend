ca202b103e8e9735240806df2c20765672e4540d
Aditya <adityakumarji74@gmail.com>
2024-01-22 11:30:39 +0530

    first commit

--- utils/features.js
+++ utils/features.js
@@ -5,7 +5,7 @@ export const sendCookie = (user, res, message, statusCode = 200) => {
   res
     .status(statusCode)
     .cookie("token", token, {
-      maxAge: 24 * 60 * 60 * 1000 * 3,
+      maxAge: 20 * 60 * 1000,
       httpOnly: true,
       sameSite: "none",
       secure: true,
